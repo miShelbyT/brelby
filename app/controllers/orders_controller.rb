9a3470300ecb8da70563a5b5307f859dabc872dd
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-20 19:12:25 -0400

    more edits?

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,54 @@
 class OrdersController < ApplicationController
+    before_action :find_order, only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @orders = Order.all
+    end
+
+    def show 
+        
+    end
+
+    def new 
+        @order = Order.new
+    end
+
+    def create 
+        @order = Order.create(order_params)
+		if @order.valid?
+			flash[:success] = "Thank you for your business! An order confirmation will be sent to your email."
+		redirect_to order_path(@order)
+		else
+			flash[:errors] = @order.errors.full_messages
+			redirect_to new_order_path			
+		end
+    end
+
+    def edit 
+
+    end
+
+    def update 
+        if @order.update(order_params)
+            redirect_to order_path(@order)
+        else
+            flash[:errors] = @order.errors.full_messages
+            redirect_to edit_order_path(@order)
+        end
+    end
+
+    def destroy 
+		@order.destroy
+		redirect_to orders_path
+    end
+
+    private
+
+	def order_params
+		params.require(:order).permit(:customer_id, :wine_id, :cupcake_id, :date, :time, :tip)
+	end
+
+	def find_order
+		@order = Order.find(params[:id])
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,21 @@
 class Order < ApplicationRecord
-  belongs_to :customer
+
+  belongs_to :user
   belongs_to :wine
   belongs_to :cupcake
+
+  validates :user_id, presence: true
+  validates :wine_id, presence: true
+  validates :cupcake_id, presence: true
+  validates :date, presence: true
+  validate :date_cannot_be_in_the_past
+  validates :time, presence: true
+  validates :tip, presence: true
+  
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
