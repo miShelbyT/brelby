241dbc814960ca585494001fec30b5a982a783a9
brynknowles <bryn.knowles@gmail.com>
2020-10-22 23:03:37 -0400

    lots of cool css and erb action

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 	resources :orders
 	resources :customers
 
-	post '/orders/:id', to: 'orders#edit'
+  post '/orders/:id', to: 'orders#edit'
+  root to: 'welcome#home'
+  
 	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
